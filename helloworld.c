e13512432408ec51a0661399b6b4ad4df768aaf3
simos <94227765+el18053@users.noreply.github.com>
2023-05-11 20:24:19 +0300

    Update helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -1,85 +1,54 @@
-// helloworld.c
-
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/resource.h>
 #include <bpf/libbpf.h>
 #include "helloworld.skel.h"
 
-typedef __u64 u64;
-typedef char stringkey[64];
-
 static int libbpf_print_fn(enum libbpf_print_level level, const char *format, va_list args)
 {
-	return vfprintf(stderr, format, args);
+    return vfprintf(stderr, format, args);
 }
 
 int main(int argc, char **argv)
 {
-	struct helloworld_bpf *skel;
-	int err;
+    struct helloworld_bpf *skel;
+    int err;
 
-	libbpf_set_strict_mode(LIBBPF_STRICT_ALL);
-	/* Set up libbpf errors and debug info callback */
-	libbpf_set_print(libbpf_print_fn);
+    libbpf_set_strict_mode(LIBBPF_STRICT_ALL);
+    /* Set up libbpf errors and debug info callback */
+    libbpf_set_print(libbpf_print_fn);
 
-	/* Open BPF application */
-	skel = helloworld_bpf__open();
-	if (!skel) {
-		fprintf(stderr, "Failed to open BPF skeleton\n");
-		return 1;
-	}   
+    /* Open BPF application */
+    skel = helloworld_bpf__open();
+    if (!skel) {
+        fprintf(stderr, "Failed to open BPF skeleton\n");
+        return 1;
+    }   
 
-	/* Load & verify BPF programs */
-	err = helloworld_bpf__load(skel);
-	if (err) {
-		fprintf(stderr, "Failed to load and verify BPF skeleton\n");
-		goto cleanup;
-	}
+    /* Load & verify BPF programs */
+    err = helloworld_bpf__load(skel);
+    if (err) {
+        fprintf(stderr, "Failed to load and verify BPF skeleton\n");
+        goto cleanup;
+    }
 
+    /* Attach tracepoint handler */
+    err = helloworld_bpf__attach(skel);
+    if (err) {
+        fprintf(stderr, "Failed to attach BPF skeleton\n");
+        goto cleanup;
+    }
 
-	/* init the counter */
-	stringkey key = "execve_counter";
-	u64 v = 0;
-	err = bpf_map__update_elem(skel->maps.execve_counter, &key, sizeof(key), &v, sizeof(v), BPF_ANY);
-	if (err != 0) {
-		fprintf(stderr, "Failed to init the counter, %d\n", err);
-		goto cleanup;
-	}
+    printf("Successfully started! Please run `sudo cat /sys/kernel/debug/tracing/trace_pipe` "
+           "to see output of the BPF programs.\n");
 
-
-	/* Attach tracepoint handler */
-	err = helloworld_bpf__attach(skel);
-	if (err) {
-		fprintf(stderr, "Failed to attach BPF skeleton\n");
-		goto cleanup;
-	}
-
-	printf("Successfully started! Please run `sudo cat /sys/kernel/debug/tracing/trace_pipe` "
-			"to see output of the BPF programs.\n");
-
-	for (;;) {
-		/* trigger our BPF program */
-		//fprintf(stderr, ".");
-		//sleep(1);
-		// read counter value from map
-		//
-		//LIBBPF_API int bpf_map__lookup_elem(const struct bpf_map *map,
-		//        const void *key, size_t key_sz,
-		//        void *value, size_t value_sz, __u64 flags);
-		//        /usr/local/bpf/include/bpf/libbpf.h
-		err = bpf_map__lookup_elem(skel->maps.execve_counter, &key, sizeof(key), &v, sizeof(v), BPF_ANY);
-		if (err != 0) {
-			fprintf(stderr, "Lookup key from map error: %d\n", err);
-			goto cleanup;
-		} 
-		else {
-			printf("execve_counter is %llu\n", v);
-		}
-		sleep(5);
-	}
+    for (;;) {
+        /* trigger our BPF program */
+        fprintf(stderr, ".");
+        sleep(1);
+    }
 
 cleanup:
-	helloworld_bpf__destroy(skel);
-	return -err;
+    helloworld_bpf__destroy(skel);
+    return -err;
 }
