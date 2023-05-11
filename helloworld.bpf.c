655b3c29a0fd4e464d74c07f3588a913c95f6276
simos <94227765+el18053@users.noreply.github.com>
2023-05-11 20:23:43 +0300

    Update helloworld.bpf.c

--- helloworld.bpf.c
+++ helloworld.bpf.c
@@ -1,34 +1,60 @@
-SEC("tracepoint/syscalls/sys_enter_pread64")
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+#include <linux/ptrace.h>
+#include <linux/types.h>
+#include <stddef.h>
+
+typedef __u32 u32;
+typedef __u64 u64;
+typedef char stringkey[64];
 
 
-int bpf_prog(struct pt_regs *ctx) {
-  char msg[] = "Hello, World!";
+//SEC("tracepoint/syscalls/sys_enter_execve")
+//SEC("tracepoint/exceptions/page_fault_kernel")
+//SEC("kprobe/add_to_page_cache_lru")
+//SEC("ksyscall/pread64")
 
-  u32 uid;
-  //uid = bpf_get_current_uid_gid() & 0xFFFFFFFF;
-  uid = bpf_get_current_pid_tgid();
-  bpf_printk("%s from process with pid: %d\n", msg, uid);
-
-
-  const u64 fd = PT_REGS_PARM1(ctx);
-  //bpf_printk("fd of the read sys call is: %d\n", fd);
-
-
-  //int byte = PT_REGS_PARM3(ctx);
-  //bpf_printk("Bytes of the read sys call is: %d\n", byte);
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__uint(max_entries, 128);
+	__type(key, stringkey);
+	__type(value, u64);
+} pid SEC(".maps");
 
 
+SEC("ksyscall/pread64")
+int trace_pread64(void *ctx) {
+	bpf_printk("Hello from ksys_pread64\n");
 
-  stringkey key = "execve_counter";
-  u64 *v = NULL;
-  v = bpf_map_lookup_elem(&execve_counter, &key);
-  if (v != NULL) {
-    //*v += 1;
-    *v = fd;
-    //bpf_map_update_elem(&execve_counter, &key, v, BPF_ANY);
-    //bpf_printk("map value: %d\n", *v);
-  }
+	/* init the counter */
+	stringkey key = "pid";
 
+	u64 uid;
+	uid = bpf_get_current_pid_tgid();
 
-  return 0;
+	int err = bpf_map_update_elem(&pid, &key, &uid, BPF_ANY);
+	if (err != 0) {
+		bpf_printk("Failed to init the counter, %d\n", err);
+		return 1;
+	}
+
+	return 0;
 }
+
+SEC("kprobe/vfs_read")
+int trace_vfs_read(void *ctx) {
+	stringkey key = "pid";
+	u64 *v = NULL; 
+	v = bpf_map_lookup_elem(&pid, &key);
+	if(v != NULL) {
+		if(*v == bpf_get_current_pid_tgid())
+			bpf_printk("Hello from vfs_read\n");
+	}
+	return 0;
+}
+
+
+char LICENSE[] SEC("license") = "Dual BSD/GPL";
+root@debian:/home/simos/libbpf-bootstrap/examples/c# 
+
