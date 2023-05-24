acf2785dfccabac2f5969cf45d015a5723ed914e
simos <94227765+el18053@users.noreply.github.com>
2023-05-24 19:49:48 +0300

    Create create_file.py

---
+++ Read/create_file.py
@@ -0,0 +1,9 @@
+import os
+
+file_size = 1024 * 1024  # 1 MB
+file_path = 'output.txt'
+
+with open(file_path, 'wb') as file:
+    file.write(os.urandom(file_size))
+
+print(f"File '{file_path}' created with size {file_size} bytes.")
