6ef1ea93d01acb7d52e05a0aa23ce9b568e5de79
simos <94227765+el18053@users.noreply.github.com>
2023-05-30 12:43:28 +0300

    Update mmap_read.c

--- Read/mmap_read.c
+++ Read/mmap_read.c
@@ -28,11 +28,28 @@ int main(int argc, char **argv) {
 		perror("mmap");
 		exit(EXIT_FAILURE);
 	}
-
+	
+	// Read the file contents so that
+	// the memory area will get accessed
+	// and the MMU will actually fetch 
+	// the data to page cache !
 	char* file_data = (char*)addr;
 
-	for (off_t i = 0; i < st.st_size; i++) {
-		printf("%c", file_data[i]);
+	for (ssize_t i=0; i < st.st_size; i++) {
+		printf("%c",file_data[i]);
+	}
+
+
+	// Read the file with pread()
+	ssize_t offset = 0; // Starting offset
+	ssize_t bytes_read;
+
+	bytes_read = pread(file_descriptor, buffer, BUFFER_SIZE, offset);
+
+	if (bytes_read == -1) {
+		perror("Failed to read the file");
+		close(file_descriptor);
+		exit(1);
 	}
 
 	// Don't forget to unmap the file after you're done
