1d9886cf53c31a2d6c96b82120c25970af0faefc
simos <94227765+el18053@users.noreply.github.com>
2023-06-05 13:06:04 +0300

    Update helloworld.bpf.c

--- BPF/helloworld.bpf.c
+++ BPF/helloworld.bpf.c
@@ -19,7 +19,7 @@ struct {
 } execve_counter SEC(".maps");
 
 
-SEC("ksyscall/pread64")
+/*SEC("ksyscall/pread64")
 
 int trace_pread64(void *ctx) {
 	stringkey pid_key = "pid";
@@ -34,13 +34,32 @@ int trace_pread64(void *ctx) {
 	}
 	return 0;
 
+}*/
+
+SEC("ksyscall/pread64")
+
+int trace_pread64(void *ctx) {
+	stringkey pid_key = "pid";
+	u32 uid;
+	uid = bpf_get_current_pid_tgid();
+
+	u32 *v;
+	v = bpf_map_lookup_elem(&execve_counter, &pid_key);
+	//if (v == NULL) {
+		bpf_printk("ksys_pread64 started from process with pid:%d\n", uid);
+		v = &uid;
+		bpf_map_update_elem(&execve_counter, &pid_key, v, BPF_ANY);
+	//}
+	
+	return 0;
+
 }
 
 SEC("kretsyscall/pread64")
 
 int trace_ret_pread64(void *ctx) {
 	stringkey pid_key = "pid";
-	u64 *v;
+	u32 *v;
 	v = bpf_map_lookup_elem(&execve_counter, &pid_key);
 	if (v != NULL) {
 		u32 uid;
@@ -57,7 +76,7 @@ SEC("kprobe/filemap_read")
 
 int trace_filemap_read(void *ctx) {
 	stringkey pid_key = "pid";
-	u64 *v;
+	u32 *v;
 	v = bpf_map_lookup_elem(&execve_counter, &pid_key);
 	if (v != NULL) {
 		u32 uid;
@@ -74,7 +93,7 @@ SEC("kprobe/filemap_get_pages")
 
 int trace_filemap_get_pages(void *ctx) {
 	stringkey pid_key = "pid";
-	u64 *v;
+	u32 *v;
 	v = bpf_map_lookup_elem(&execve_counter, &pid_key);
 	if (v != NULL) {
 		u32 uid;
@@ -91,7 +110,7 @@ SEC("kprobe/filemap_get_read_batch")
 
 int trace_filemap_get_read_batch(void *ctx) {
 	stringkey pid_key = "pid";
-	u64 *v;
+	u32 *v;
 	v = bpf_map_lookup_elem(&execve_counter, &pid_key);
 	if (v != NULL) {
 		u32 uid;
@@ -110,14 +129,14 @@ SEC("kprobe/page_cache_sync_ra")
 int trace_page_cache_sync_ra_enter(struct pt_regs *ctx)
 {
 	stringkey pid_key = "pid";
-	u64 *v;
+	u32 *v;
 	v = bpf_map_lookup_elem(&execve_counter, &pid_key);
 	if (v != NULL) {
 		u32 uid;
 		uid = bpf_get_current_pid_tgid();
 		if (*v == uid) {
 			//page_cache_sync_ra started!
-			bpf_printk("page_cache_sync_ra started\n");
+			//bpf_printk("page_cache_sync_ra started\n");
 			stringkey sync_ra_key = "sync_ra";
 			u64 myValue = 1;
 			u64* v = &myValue;
@@ -133,16 +152,16 @@ SEC("kretprobe/page_cache_sync_ra")
 int trace_page_cache_sync_ra_exit(struct pt_regs *ctx)
 {
 	stringkey pid_key = "pid";
-	u64 *v;
+	u32 *v;
 	v = bpf_map_lookup_elem(&execve_counter, &pid_key);
 	if (v != NULL) {
 		u32 uid;
 		uid = bpf_get_current_pid_tgid();
 		if (*v == uid) {
 			//page_cache_sync_ra exits!
-			bpf_printk("page_cache_sync_ra finished\n");
+			//bpf_printk("page_cache_sync_ra finished\n");
 			stringkey sync_ra_key = "sync_ra";
-			v = NULL;
+			u64 *v = NULL;
 			v = bpf_map_lookup_elem(&execve_counter, &sync_ra_key);
 			if (v != NULL) {
 				*v = 0;
@@ -157,14 +176,14 @@ SEC("kprobe/page_cache_async_ra")
 int trace_page_cache_async_ra_enter(struct pt_regs *ctx)
 {
 	stringkey pid_key = "pid";
-	u64 *v;
+	u32 *v;
 	v = bpf_map_lookup_elem(&execve_counter, &pid_key);
 	if (v != NULL) {
 		u32 uid;
 		uid = bpf_get_current_pid_tgid();
 		if (*v == uid) {
 			//page_cache_async_ra started!
-			bpf_printk("page_cache_async_ra started\n");
+			//bpf_printk("page_cache_async_ra started\n");
 			stringkey sync_ra_key = "async_ra";
 			u64 myValue = 1;
 			u64* v = &myValue;
@@ -180,16 +199,16 @@ SEC("kretprobe/page_cache_async_ra")
 int trace_page_cache_async_ra_exit(struct pt_regs *ctx)
 {
 	stringkey pid_key = "pid";
-	u64 *v;
+	u32 *v;
 	v = bpf_map_lookup_elem(&execve_counter, &pid_key);
 	if (v != NULL) {
 		u32 uid;
 		uid = bpf_get_current_pid_tgid();
 		if (*v == uid) {
 			//page_cache_async_ra exits!
-			bpf_printk("page_cache_async_ra finished\n");
+			//bpf_printk("page_cache_async_ra finished\n");
 			stringkey sync_ra_key = "async_ra";
-			v = NULL;
+			u64 *v = NULL;
 			v = bpf_map_lookup_elem(&execve_counter, &sync_ra_key);
 			if (v != NULL) {
 				*v = 0;
@@ -203,7 +222,7 @@ SEC("kprobe/ondemand_readahead")
 
 int trace_ondemand_readahead(void *ctx) {
 	stringkey pid_key = "pid";
-	u64 *v;
+	u32 *v;
 	v = bpf_map_lookup_elem(&execve_counter, &pid_key);
 	if (v != NULL) {
 		u32 uid;
@@ -220,7 +239,7 @@ SEC("kprobe/do_page_cache_ra")
 
 int trace_do_page_cache_ra(void *ctx) {
 	stringkey pid_key = "pid";
-	u64 *v;
+	u32 *v;
 	v = bpf_map_lookup_elem(&execve_counter, &pid_key);
 	if (v != NULL) {
 		u32 uid;
@@ -237,7 +256,7 @@ SEC("kprobe/page_cache_ra_unbounded")
 
 int trace_page_cache_ra_unbounded(void *ctx) {
 	stringkey pid_key = "pid";
-	u64 *v;
+	u32 *v;
 	v = bpf_map_lookup_elem(&execve_counter, &pid_key);
 	if (v != NULL) {
 		u32 uid;
@@ -257,14 +276,14 @@ SEC("kprobe/add_to_page_cache_lru")
 int trace_page_cache_lru(struct pt_regs *ctx)
 {
 	stringkey pid_key = "pid";
-	u64 *v;
+	u32 *v;
 	v = bpf_map_lookup_elem(&execve_counter, &pid_key);
 	if (v != NULL) {
 		u32 uid;
 		uid = bpf_get_current_pid_tgid();
 		if (*v == uid) {
 			stringkey sync_ra_key = "sync_ra";
-			v = NULL;
+			u64 *v = NULL;
 			v = bpf_map_lookup_elem(&execve_counter, &sync_ra_key);
 			if (v != NULL && *v == 1) {
 				stringkey new_key = "sync_accessed";
@@ -300,18 +319,18 @@ SEC("kretprobe/copy_page_to_iter")
 int trace_copy_page_to_iter(struct pt_regs *ctx)
 {
 	stringkey pid_key = "pid";
-	u64 *v;
+	u32 *v;
 	v = bpf_map_lookup_elem(&execve_counter, &pid_key);
 	if (v != NULL) {
 		u32 uid;
 		uid = bpf_get_current_pid_tgid();
 		if (*v == uid) {
-			bpf_printk("copy_page_to_iter started\n");
+			//bpf_printk("copy_page_to_iter started\n");
 			stringkey new_key = "copy_page_to_iter";
-			v = NULL;
+			u64 *v = NULL;
 			v = bpf_map_lookup_elem(&execve_counter, &new_key);
 			if (v != NULL) {
-				if ( PT_REGS_RC(ctx) )
+				//if ( PT_REGS_RC(ctx) )
 					*v += 1;
 			}
 		}
@@ -323,18 +342,18 @@ int trace_copy_page_to_iter(struct pt_regs *ctx)
 
 SEC("kprobe/mark_page_accessed")
 
-int trace__(struct pt_regs *ctx)
+int trace_mark_page_accessed(struct pt_regs *ctx)
 {
 	stringkey pid_key = "pid";
-	u64 *v;
+	u32 *v;
 	v = bpf_map_lookup_elem(&execve_counter, &pid_key);
 	if (v != NULL) {
 		u32 uid;
 		uid = bpf_get_current_pid_tgid();
 		if (*v == uid) {
-			bpf_printk("mark_page_accessed started\n");
+			//bpf_printk("mark_page_accessed started\n");
 			stringkey new_key = "mark_page_accessed";
-			v = NULL;
+			u64 *v = NULL;
 			v = bpf_map_lookup_elem(&execve_counter, &new_key);
 			if (v != NULL) {
 				*v += 1;
