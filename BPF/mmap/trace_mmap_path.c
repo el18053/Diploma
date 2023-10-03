b8e99a2bd10eb6479a87d04a30be06b8b28dd998
simos <94227765+el18053@users.noreply.github.com>
2023-10-03 13:42:58 +0300

    Update trace_mmap_path.c

--- BPF/mmap/trace_mmap_path.c
+++ BPF/mmap/trace_mmap_path.c
@@ -55,124 +55,151 @@ int main(int argc, char **argv)
 	printf("Successfully started! Please run `sudo cat /sys/kernel/debug/tracing/trace_pipe` "
 			"to see output of the BPF programs.\n");
 
+	int file_size = 20;
+	pid_t child_pid = fork();
 
-	int file_size = 512;
-	sleep(1);
-
-	stringkey pid_key = "pid";
-	int pid = getpid();
-	err = bpf_map__update_elem(skel->maps.execve_counter, &pid_key, sizeof(pid_key), &pid, sizeof(pid),  BPF_ANY);
-	if (err != 0) {
-		fprintf(stderr, "Failed to save key %d\n", err);
+	if (child_pid == -1) {
+		fprintf(stderr, "Fork failed");
 		goto cleanup;
 	}
 
-	stringkey key = "key";
-	int init_key = 0;
-	err = bpf_map__update_elem(skel->maps.execve_counter, &key, sizeof(key), &init_key, sizeof(init_key),  BPF_ANY);
-	if (err != 0) {
-		fprintf(stderr, "Failed to save key %d\n", err);
-		goto cleanup;
-	}
-
-	stringkey bring_page_key = "bring_page";
-	int bring_page = 1;
-	err = bpf_map__update_elem(skel->maps.execve_counter, &bring_page_key, sizeof(bring_page_key), &bring_page, sizeof(bring_page),  BPF_ANY);
-	if (err != 0) {
-		fprintf(stderr, "Failed to save key %d\n", err);
-		goto cleanup;
-	}
-
-	int i = 0, nr_pages = file_size / 4;
-
-	err = bpf_map__update_elem(skel->maps.index_map, &i, sizeof(i), &nr_pages, sizeof(nr_pages), BPF_ANY);
-	if (err != 0) {
-		fprintf(stderr, "Failed to create indexes array err=%d", err);
-		goto cleanup;
-	}
-
-	for(i = 0; i < nr_pages; i++) 
+	if( child_pid == 0)
 	{
-		int j = i + 1;
-		err = bpf_map__update_elem(skel->maps.index_map, &j, sizeof(j), &i, sizeof(i), BPF_ANY);
+		sleep(1);
+		int bs = 4; //bs stands for block size
+		int fs = file_size; //fs stands for file size
+		int rs = fs; //rs stands for how many bytes of the file do we want to read (bs <= rs <= fs)
+
+		char fioCommand[100];
+		sprintf(fioCommand, "run_fio.sh");
+		//execl("./", fioCommand, NULL);
+		int error = execl("/home/simos/libbpf-bootstrap/examples/c/", "ls", NULL);
+
+		perror("Exec failed");
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+
+		stringkey pid_key = "pid";
+		int pid = child_pid;
+		err = bpf_map__update_elem(skel->maps.execve_counter, &pid_key, sizeof(pid_key), &pid, sizeof(pid),  BPF_ANY);
+		if (err != 0) {
+			fprintf(stderr, "Failed to save key %d\n", err);
+			goto cleanup;
+		}
+
+		stringkey key = "key";
+		int init_key = 0;
+		err = bpf_map__update_elem(skel->maps.execve_counter, &key, sizeof(key), &init_key, sizeof(init_key),  BPF_ANY);
+		if (err != 0) {
+			fprintf(stderr, "Failed to save key %d\n", err);
+			goto cleanup;
+		}
+
+		stringkey bring_page_key = "bring_page";
+		int bring_page = 0;
+		err = bpf_map__update_elem(skel->maps.execve_counter, &bring_page_key, sizeof(bring_page_key), &bring_page, sizeof(bring_page),  BPF_ANY);
+		if (err != 0) {
+			fprintf(stderr, "Failed to save key %d\n", err);
+			goto cleanup;
+		}
+		
+		int i = 0, nr_pages = file_size / 4;
+
+		err = bpf_map__update_elem(skel->maps.index_map, &i, sizeof(i), &nr_pages, sizeof(nr_pages), BPF_ANY);
 		if (err != 0) {
 			fprintf(stderr, "Failed to create indexes array err=%d", err);
 			goto cleanup;
 		}
-	}
 
-	int bs = 4; //bs stands for block size
-	int fs = file_size; //fs stands for file size
-	int rs = fs; //rs stands for how many bytes of the file do we want to read (bs <= rs <= fs)
+		for(i = 0; i < nr_pages; i++) 
+		{
+			int j = i + 1;
+			err = bpf_map__update_elem(skel->maps.index_map, &j, sizeof(j), &i, sizeof(i), BPF_ANY);
+			if (err != 0) {
+				fprintf(stderr, "Failed to create indexes array err=%d", err);
+				goto cleanup;
+			}
+		}
 
-	//Empty Cache
-	int result = system("echo 1 > /proc/sys/vm/drop_caches");
+		//Empty Cache
+		int result = system("echo 1 > /proc/sys/vm/drop_caches");
 
-	if (result == -1) {
-		printf("Failed to empty cache.\n");
-		goto cleanup;
-	}
+		if (result == -1) {
+			printf("Failed to empty cache.\n");
+			goto cleanup;
+		}
 
-	const char* file_path = "output.txt";
-	int file_descriptor;
-	char buffer[BUFFER_SIZE];
+		int status;
+        	waitpid(child_pid, &status, 0);
 
-	// Open the file
-	file_descriptor = open(file_path, O_RDONLY);
-	if (file_descriptor == -1) {
+        	if (WIFEXITED(status)) {
+            		printf("Child process exited with status: %d\n", WEXITSTATUS(status));
+        	}
+
+		//Delete test file created by fio (it creates future hazzards)
+		result = system("rm test");
+
+		if (result == -1) {
+			printf("Failed to empty cache.\n");
+			goto cleanup;
+		}
+
+		/*const char* file_path = "output.txt";
+		  int file_descriptor;
+		  char buffer[BUFFER_SIZE];
+
+		// Open the file
+		file_descriptor = open(file_path, O_RDONLY);
+		if (file_descriptor == -1) {
 		fprintf(stderr, "Failed to open the file");
 		goto cleanup;
-	}
+		}
 
-	//mmap the file
-	struct stat st;
-	fstat(file_descriptor, &st); //obtain file size
+		//mmap the file
+		struct stat st;
+		fstat(file_descriptor, &st); //obtain file size
 
-	void *addr = mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, file_descriptor, 0);
-	if (addr == MAP_FAILED) {
+		void *addr = mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE, file_descriptor, 0);
+		if (addr == MAP_FAILED) {
 		printf("mmap failed \n");
 		goto cleanup;
-	}
+		}
 
-	char* file_data = (char*)addr;
+		char* file_data = (char*)addr;
 
-	for (ssize_t i=0; i < st.st_size; i++) {
+		for (ssize_t i=0; i < st.st_size; i++) {
 		char letter = file_data[i];
+		}
+
+		// Don't forget to unmap the file after you're done
+		munmap(addr, st.st_size);
+		*/
+
+		int key_size;
+		err = bpf_map__lookup_elem(skel->maps.execve_counter, &key, sizeof(key),  &key_size, sizeof(key_size), BPF_ANY);
+		if (err != 0) {
+			fprintf(stderr, "Failed to retreive key_size, err= %d\n", err);
+			goto cleanup;
+		}
+
+		FILE* file = fopen("log.txt", "w");
+		if (file == NULL) {
+			printf("Failed to open the file.\n");
+			goto cleanup;
+		}
+
+		for (int i=0; i < key_size; i++) {
+			stringinput message;
+			err = bpf_map__lookup_elem(skel->maps.log_file, &i, sizeof(i), message, sizeof(message), BPF_ANY);
+			fprintf(file, "%s\n", message);
+		}
+
+		fclose(file);
+
+		sleep(1);
 	}
-
-	// Don't forget to unmap the file after you're done
-	munmap(addr, st.st_size);
-
-
-	int key_size;
-	err = bpf_map__lookup_elem(skel->maps.execve_counter, &key, sizeof(key),  &key_size, sizeof(key_size), BPF_ANY);
-	if (err != 0) {
-		fprintf(stderr, "Failed to retreive key_size, err= %d\n", err);
-		goto cleanup;
-	}
-
-	FILE* file = fopen("log.txt", "w");
-	if (file == NULL) {
-		printf("Failed to open the file.\n");
-		goto cleanup;
-	}
-
-	for (int i=0; i < key_size; i++) {
-		stringinput message;
-		err = bpf_map__lookup_elem(skel->maps.log_file, &i, sizeof(i), message, sizeof(message), BPF_ANY);
-		fprintf(file, "%s\n", message);
-	}
-
-	fclose(file);
-
-	/*result = system("cat log.txt | grep add");
-	if (result == -1)
-	{
-		printf("error: cat log.txt\n");
-		goto cleanup;
-	}*/
-
-	sleep(1);
 
 cleanup:
 	trace_mmap_path_bpf__destroy(skel);
