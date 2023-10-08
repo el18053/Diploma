78a06b232864d0a51587b5184c73ae1f8780065f
simos <94227765+el18053@users.noreply.github.com>
2023-10-08 13:53:42 +0300

    Update trace_mmap_path.bpf.c

--- BPF/mmap/trace_mmap_path.bpf.c
+++ BPF/mmap/trace_mmap_path.bpf.c
@@ -44,6 +44,21 @@ int get_access(u32 process_pid)
 	return 0;
 }
 
+/*int get_access(stringkey comm)
+{
+	if (comm != NULL)
+	{
+		if(comm[0] == 102 && comm[1] == 105 && comm[2] == 111)
+		{
+			return 1;
+		}
+		
+		return 0;
+	}
+
+	return 0;
+}*/
+
 int get_key() {
 	stringkey key = "key";
 	int *key_val = bpf_map_lookup_elem(&execve_counter, &key);
@@ -58,11 +73,12 @@ int get_key() {
 SEC("kprobe/filemap_fault")
 
 int trace_filemap_fault(struct pt_regs *ctx) {
-	//stringkey comm;
-	//bpf_get_current_comm(&comm, sizeof(comm));
+	stringkey comm;
+	bpf_get_current_comm(&comm, sizeof(comm));
 	
 	if ( get_access(bpf_get_current_pid_tgid()) )
 	{
+
 		bpf_printk("filemap_fault started");
 
 		stringinput message;
@@ -70,8 +86,11 @@ int trace_filemap_fault(struct pt_regs *ctx) {
 		bpf_probe_read_str(message, sizeof(message), "filemap_fault started");
 		bpf_map_update_elem(&log_file, &key, message, BPF_ANY);
 		
-		struct vm_fault *vmf = (struct vm_fault *)PT_REGS_PARM1(ctx);
-
+		struct vm_fault vmf;// = (struct vm_fault *)PT_REGS_PARM1(ctx);
+		bpf_probe_read(&vmf, sizeof(struct vm_fault), (struct vm_fault *)PT_REGS_PARM1(ctx));
+		struct vm_area_struct *vma = vmf.vma;
+		struct file **filp = &vma->vm_file;
+		
 		stringkey bring_page_key = "bring_page";
 		int *bring_pages = bpf_map_lookup_elem(&execve_counter, &bring_page_key);
 		if (bring_pages != NULL)
@@ -79,7 +98,7 @@ int trace_filemap_fault(struct pt_regs *ctx) {
 			if (*bring_pages == 0)
 			{
 				*bring_pages = 1;
-				bpf_simos(vmf, &index_map);
+				bpf_simos(filp, &index_map);
 			}
 		}	
 	}	
@@ -90,7 +109,9 @@ int trace_filemap_fault(struct pt_regs *ctx) {
 SEC("kprobe/pagecache_get_page")
 
 int trace_pagecache_get_page(struct pt_regs *ctx) {
-
+	stringkey comm;
+	bpf_get_current_comm(&comm, sizeof(comm));
+	
 	if ( get_access(bpf_get_current_pid_tgid()) )
 	{
 		bpf_printk("pagecache_get_page started");
@@ -107,7 +128,9 @@ int trace_pagecache_get_page(struct pt_regs *ctx) {
 SEC("kretprobe/pagecache_get_page")
 
 int trace_ret_pagecache_get_page(struct pt_regs *ctx) {
-
+	stringkey comm;
+	bpf_get_current_comm(&comm, sizeof(comm));
+	
 	if ( get_access(bpf_get_current_pid_tgid()) )
 	{
 		bpf_printk("pagecache_get_page exited");
@@ -125,6 +148,9 @@ SEC("kprobe/page_cache_sync_ra")
 
 int trace_page_cache_sync_ra_enter(struct pt_regs *ctx)
 {
+	stringkey comm;
+	bpf_get_current_comm(&comm, sizeof(comm));
+	
 	if ( get_access(bpf_get_current_pid_tgid()) )
 	{
 		//page_cache_sync_ra started!
@@ -145,6 +171,9 @@ SEC("kretprobe/page_cache_sync_ra")
 
 int trace_page_cache_sync_ra_exit(struct pt_regs *ctx)
 {
+	stringkey comm;
+	bpf_get_current_comm(&comm, sizeof(comm));
+	
 	if ( get_access(bpf_get_current_pid_tgid()) )
 	{
 		//page_cache_sync_ra exits!
@@ -163,6 +192,9 @@ SEC("kprobe/page_cache_async_ra")
 
 int trace_page_cache_async_ra_enter(struct pt_regs *ctx)
 {
+	stringkey comm;
+	bpf_get_current_comm(&comm, sizeof(comm));
+	
 	if ( get_access(bpf_get_current_pid_tgid()) )
 	{
 		//page_cache_async_ra started!
@@ -183,6 +215,9 @@ SEC("kretprobe/page_cache_async_ra")
 
 int trace_page_cache_async_ra_exit(struct pt_regs *ctx)
 {
+	stringkey comm;
+	bpf_get_current_comm(&comm, sizeof(comm));
+	
 	if ( get_access(bpf_get_current_pid_tgid()) )
 	{
 		//page_cache_async_ra exits!
@@ -199,7 +234,11 @@ int trace_page_cache_async_ra_exit(struct pt_regs *ctx)
 
 SEC("kprobe/do_page_cache_ra")
 
-int trace_do_page_cache_ra(struct pt_regs *ctx) {
+int trace_do_page_cache_ra(struct pt_regs *ctx) 
+{
+	stringkey comm;
+	bpf_get_current_comm(&comm, sizeof(comm));
+	
 	if ( get_access(bpf_get_current_pid_tgid()) )
 	{
 		int req_size = PT_REGS_PARM2(ctx);
@@ -217,7 +256,11 @@ int trace_do_page_cache_ra(struct pt_regs *ctx) {
 
 SEC("kprobe/page_cache_ra_unbounded")
 
-int trace_page_cache_ra_unbounded(struct pt_regs *ctx) {
+int trace_page_cache_ra_unbounded(struct pt_regs *ctx) 
+{
+	stringkey comm;
+	bpf_get_current_comm(&comm, sizeof(comm));
+	
 	if ( get_access(bpf_get_current_pid_tgid()) )
 	{
 		int nr_to_read = PT_REGS_PARM2(ctx);
@@ -236,7 +279,11 @@ int trace_page_cache_ra_unbounded(struct pt_regs *ctx) {
 
 SEC("kretprobe/page_cache_ra_unbounded")
 
-int trace_page_cache_ra_unbounded_exit(struct pt_regs *ctx) {
+int trace_page_cache_ra_unbounded_exit(struct pt_regs *ctx) 
+{
+	stringkey comm;
+	bpf_get_current_comm(&comm, sizeof(comm));
+	
 	if ( get_access(bpf_get_current_pid_tgid()) )
 	{
 		bpf_printk("page_cache_ra_unbounded exited");
@@ -254,11 +301,14 @@ SEC("kprobe/add_to_page_cache_lru")
 
 int trace_page_cache_lru(struct pt_regs *ctx)
 {
+	stringkey comm;
+	bpf_get_current_comm(&comm, sizeof(comm));
+	
 	if ( get_access(bpf_get_current_pid_tgid()) )
 	{
 		int offset = PT_REGS_PARM3(ctx);
 
-		//bpf_printk("add_to_page_cache_lru started with offset : %d", offset);
+		bpf_printk("add_to_page_cache_lru started with offset : %d", offset);
 
 		stringinput message = "";
 		int key = get_key();
@@ -268,6 +318,5 @@ int trace_page_cache_lru(struct pt_regs *ctx)
 
 	return 0;
 }
-
 
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
